7e0f318b884746c798a264d2566175089459cf45
Ivan Kirshak <i.kirshak@gmail.com>
2020-06-05 16:02:27 +0300

    added from processing

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const menuBtn = document.getElementById("menuBtn");
 const navigation = document.querySelector(".navigation__inner-container");
-const formSubmitBtn = document.getElementById("formSubmitBtn");
 const formAdvanced = document.getElementById("formAdvanced");
+
 
 function mobileMenu() {
     menuBtn.classList.toggle("change");
@@ -13,3 +13,45 @@ function mobileMenu() {
     }
 }
 menuBtn.addEventListener("click", mobileMenu, false);
+
+formAdvanced.onsubmit = function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    // console.log(formData);
+
+    const link = formData.get("link");
+    addLink(link);
+
+    formAdvanced.reset()
+}
+
+function copyLink(event) {
+    // console.log(event.target.previousSibling.previousSibling.innerText);
+
+    let linkToCopy = event.target.previousSibling.previousSibling.innerText;
+    navigator.clipboard.writeText(linkToCopy);
+    event.target.style.backgroundColor = "hsl(257, 27%, 26%)";
+    event.target.innerHTML = "Copied!";
+}
+
+function addListItem(link) {
+    return `<div class="list__inner">
+                <h3 class="prev-link">${link}</h3>
+                <h3 class="short-link">${link}</h3>
+                <button 
+                type="button" 
+                class="copy-button"
+                onclick="copyLink(event)"
+                >Copy</button>
+            </div>`;
+}
+
+function addLink(link) {
+    const linksList = document.getElementById("links");
+
+    const linkListItem = document.createElement("li");
+    linkListItem.innerHTML = addListItem(link);
+
+    linksList.prepend(linkListItem);
+}
