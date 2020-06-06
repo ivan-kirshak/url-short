fb41879523ef8278b4120b5a88d2c4b1a52c676a
Ivan Kirshak <i.kirshak@gmail.com>
2020-06-06 18:30:58 +0300

    styles & start shortener functionality development

--- js/index.js
+++ js/index.js
@@ -29,6 +29,7 @@ formAdvanced.onsubmit = function (event) {
         error.innerHTML = "Please add a link";
     } else {
         addLink(link);
+        processLink(link);
         urlInput.style.border = "1px solid black";
         urlInput.style.color = "gray";
         error.innerHTML = "";
@@ -45,6 +46,18 @@ function copyLink(event) {
     navigator.clipboard.writeText(linkToCopy);
     event.target.style.backgroundColor = "hsl(257, 27%, 26%)";
     event.target.innerHTML = "Copied!";
+}
+
+function processLink(link) {
+    console.log("processing");
+    let URI = `https://rel.ink/api/links/${link}`;
+    const XHR = new XMLHttpRequest();
+    if ((XHR.readyState === 4) && (XHR.status === 200)) {
+        let result = JSON.parse(XHR.responseText);
+        console.log(result);
+    }
+    XHR.open("POST", URI);
+    XHR.send();
 }
 
 function addListItem(link) {
